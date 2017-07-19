3dfc6a8558b014ca4563beeb732393d3464fdc07
Riva Syafri Rachmatullah <riva@xtremax.com>
2017-07-19 16:38:20 +0700

    setting up eslint and splitting up webpack config to parts

---
+++ webpack.parts.js
@@ -0,0 +1,64 @@
+const webpack = require('webpack');
+
+exports.devServer = ({ host, port } = {}) => ({
+  devServer: {
+    // Enable history API fallback so HTML5 History API based
+    // routing works. Good for complex setups.
+    historyApiFallback: true,
+
+    // Display only errors to reduce the amount of output.
+    stats: 'errors-only',
+    
+    // Parse host and port from env to allow customization.
+    //
+    // If you use Docker, Vagrant or Cloud9, set
+    // host: options.host || '0.0.0.0';
+    //
+    // 0.0.0.0 is available to all network devices
+    // unlike default `localhost`.
+    host, // Defaults to `localhost`
+    port, // Defaults to 8080
+
+    // overlay: true is equivalent
+    overlay: {
+      errors: true,
+      warnings: true,
+    }
+  }
+});
+
+exports.lintJavaScript = ({ include, exclude, options }) => ({
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include,
+        exclude,
+        enforce: 'pre',
+
+        loader: 'eslint-loader',
+        options,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        eslint: {
+          // Fail only on errors
+          failOnWarning: false,
+          failOnError: true,
+
+          // Toggle autofix
+          fix: false,
+
+          // Output to Jenkins compatible XML
+          outputReport: {
+            filePath: 'checkstyle.xml',
+            formatter: require('eslint/lib/formatters/checkstyle'),
+          }
+        }
+      }
+    })
+  ]
+});
